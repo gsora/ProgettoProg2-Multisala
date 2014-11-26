8ff6fbc9e8d9bd27405658710996636d267a119a
Gianguido Sorà <gianguidorama@gmail.com>
2014-11-26 13:38:03 +0100

    primo test per il multisala

---
+++ src/it/unisa/prog2/multisala/TestMultisala.java
@@ -0,0 +1,17 @@
+package it.unisa.prog2.multisala;
+
+public class TestMultisala {
+
+	public static void main(String[] args) throws OrarioNonValidoException, DataNonValidaException {
+		
+		Spettacolo ggpas = new Spettacolo("Guida Galattica Per Autostoppisti", 42, "18:43", "1/2/2014", 2.6);
+		Sala s = new Sala(1, ggpas);
+		
+		System.out.println(s.statoPostiSala());
+		
+		s.compraBiglietto();
+		
+		System.out.println(s.statoPostiSala());
+	}
+
+}
