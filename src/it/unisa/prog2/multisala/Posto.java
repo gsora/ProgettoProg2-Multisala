2a1a379730271c988e0afb9fcd091f9bcf56ca09
Gianguido Sorà <gianguidorama@gmail.com>
2014-11-26 11:48:29 +0100

    aggiunta definizione di Posto e una classe Eccezione nel caso di status posto non valido

---
+++ src/it/unisa/prog2/multisala/Posto.java
@@ -0,0 +1,86 @@
+package it.unisa.prog2.multisala;
+
+public class Posto {
+
+	// numero identificativo del posto
+	private int numeroPosto;
+	
+	// 0 = libero; 1 = assegnato; 2 = prenotato
+	private int status;
+	
+	
+	/**
+	 * Costruttore della classe Posto
+	 * @param np numero del posto assegnato
+	 */
+	
+	public Posto(int np) {
+		numeroPosto = np;
+		status = 0;
+	}
+	
+	public Posto(int np, int ns) {
+		numeroPosto = np;
+		
+		try {
+			checkStatus(ns);
+			status = ns;
+		} catch (StatusPostoNonValidoException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Ritorna numero del posto
+	 * @return numero posto
+	 */
+	
+	public int getNumeroPosto() {
+		return numeroPosto;
+	}
+	
+	/**
+	 * Ritorna lo status del posto.
+	 * 0 = libero
+	 * 1 = assegnato
+	 * 2 = prenotato
+	 * @return status del posto
+	 */
+	
+	public int getStatus() {
+		return status;
+	}
+	
+	/**
+	 * Setta lo status del posto
+	 * @param ns status del posto
+	 */
+	
+	public void setStatus(int ns) {
+		try {
+			checkStatus(ns);
+			status = ns;
+		} catch (StatusPostoNonValidoException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Setta il numero del posto
+	 * @param np numero del posto
+	 */
+	
+	public void setNumeroPosto(int np) {
+		numeroPosto = np;
+	}
+	
+	
+	// controlla che lo status passato sia valido
+	private void checkStatus(int ns) throws StatusPostoNonValidoException {
+		if(ns < 0 || ns > 2) {
+			throw new StatusPostoNonValidoException("lo status deve essere compreso tra 0 e 2.");
+		}
+	}
+	
+	
+}
---
+++ src/it/unisa/prog2/multisala/StatusPostoNonValidoException.java
@@ -0,0 +1,14 @@
+package it.unisa.prog2.multisala;
+
+@SuppressWarnings("serial")
+public class StatusPostoNonValidoException extends Exception {
+	
+	public StatusPostoNonValidoException() {
+		
+	}
+	
+	public StatusPostoNonValidoException(String message) {
+		super(message);
+	}
+
+}
