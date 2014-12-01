87811f9b393cc7d0eb7bf4ba5969a815b623355e
Gianguido Sorà <gianguidorama@gmail.com>
2014-12-01 11:45:00 +0100

    aggiunta eccezione lanciata nel caso in cui i posti liberi nella sala siano finiti

---
+++ src/it/unisa/prog2/multisala/PostiLiberiEsauritiException.java
@@ -0,0 +1,14 @@
+package it.unisa.prog2.multisala;
+
+@SuppressWarnings("serial")
+public class PostiLiberiEsauritiException extends Exception {
+	
+	public PostiLiberiEsauritiException() {
+		
+	}
+	
+	public PostiLiberiEsauritiException(String message) {
+		super(message);
+	}
+
+}
