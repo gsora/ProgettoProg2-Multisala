cea803541c00575174fdc3428ac2adaf707e391a
Gianguido Sorà <gianguidorama@gmail.com>
2014-12-01 11:56:40 +0100

    aggiunta eccezione lanciata nel caso in cui i posti prenotabili sono esauriti

---
+++ src/it/unisa/prog2/multisala/PostiPrenotabiliEsauritiException.java
@@ -0,0 +1,14 @@
+package it.unisa.prog2.multisala;
+
+@SuppressWarnings("serial")
+public class PostiPrenotabiliEsauritiException extends Exception {
+	
+	public PostiPrenotabiliEsauritiException() {
+		
+	}
+	
+	public PostiPrenotabiliEsauritiException(String message) {
+		super(message);
+	}
+
+}
