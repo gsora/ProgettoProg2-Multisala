9d2e5ebf39ff16b0e37ccbd01c286f058af2a124
Gianguido Sorà <gianguidorama@gmail.com>
2014-12-01 11:58:05 +0100

    pseudo-classe di test che fa da multisala

--- src/it/unisa/prog2/multisala/TestMultisala.java
+++ src/it/unisa/prog2/multisala/TestMultisala.java
@@ -2,7 +2,7 @@ package it.unisa.prog2.multisala;
 
 public class TestMultisala {
 
-	public static void main(String[] args) throws OrarioNonValidoException, DataNonValidaException {
+	public static void main(String[] args) throws OrarioNonValidoException, DataNonValidaException, PostiLiberiEsauritiException {
 		
 		Spettacolo ggpas = new Spettacolo("Guida Galattica Per Autostoppisti", 42, "18:43", "1/2/2014", 2.6);
 		Sala s = new Sala(1, ggpas);
@@ -12,6 +12,63 @@ public class TestMultisala {
 		s.compraBiglietto();
 		
 		System.out.println(s.statoPostiSala());
+		
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		
+		System.out.println(s.statoPostiSala());
+		
 	}
 
 }
