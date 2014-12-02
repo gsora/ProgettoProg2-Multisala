e8e6c6d3490674212fa40ecdda501be7ab992b36
Gianguido Sorà <gianguidorama@gmail.com>
2014-12-02 17:59:48 +0100

    aggiunto metodo ricaricaUI che ridisegna in modo corretto le caselle

--- src/it/unisa/prog2/multisala/TestMultisala.java
+++ src/it/unisa/prog2/multisala/TestMultisala.java
@@ -2,8 +2,9 @@ package it.unisa.prog2.multisala;
 
 import javax.swing.*;
 
-import java.awt.*;
+import sun.awt.windows.ThemeReader;
 
+import java.awt.*;
 
 public class TestMultisala {
 
@@ -12,39 +13,43 @@ public class TestMultisala {
 		Spettacolo ggpas = new Spettacolo("Guida Galattica Per Autostoppisti", 42, "18:43", "1/2/2014", 2.6);
 		Sala s = new Sala(1, ggpas);
 		
-		System.out.println(s.statoPostiSala());
-		
-		s.compraBiglietto();
-		s.compraBiglietto();
-		s.compraBiglietto();
-		s.compraBiglietto();
-		s.compraBiglietto();
-		s.compraBiglietto();
-		s.compraBiglietto();
-		s.compraBiglietto();
-		s.compraBiglietto();
-		
-		System.out.println(s.statoPostiSala());
-		
-		
 		JFrame frame = new JFrame();
-		
 		frame.setSize(1150, 720);
+		frame.setVisible(true);
 		frame.setResizable(false);
 		frame.setTitle("Test");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new GridLayout(5, 10));
+		frame.setLayout(new GridLayout(5, 10, 50, 50));
+		frame.setLocationRelativeTo(null);
+		
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto();
+		s.compraBiglietto(47);
 		
 		DrawPosto[] a = new DrawPosto[50];
 		
+		ricaricaUI(frame, s, a);
+		
+	}
+	
+	private static void ricaricaUI(JFrame frame, Sala s, DrawPosto[] a) {
+		
+		frame.getContentPane().removeAll();
 		
 		for(int i = 0; i < s.getNumeroPostiTotali(); i++) {
-			a[i] = new DrawPosto(s.getStatoPostoSingolo(i));
-			frame.repaint();
+			a[i] = new DrawPosto(s.getStatoPostoSingolo(i), i+1);
 			frame.add(a[i]);
 		}
-		frame.setVisible(true);
 		
+		frame.revalidate();
+		frame.repaint();
 	}
 
 }
