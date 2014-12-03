758f886212b1c01f3e19f4d33d2cfffc29386a59
Carmine Vincenzo Russo <carmine94@hotmail.it>
2014-12-03 18:47:06 +0100

    Aggiunta classe per l'accoglienza grafica dell'utente

---
+++ src/it/unisa/prog2/multisala/Entrata.java
@@ -0,0 +1,51 @@
+package it.unisa.prog2.multisala;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class Entrata {
+
+	private JFrame entrata;
+	private JButton nuovoUtente;
+	private JButton utenteEsistente;
+	private JPanel pan;
+	private JLabel messaggioEntrata;
+	
+	public Entrata(String nomeCinema){
+		entrata = new JFrame();
+		entrata.setSize(300, 150);
+		entrata.setResizable(false);
+		entrata.setLocationRelativeTo(null);
+		entrata.setVisible(true);
+		entrata.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		entrata.setTitle(nomeCinema);
+		entrata.setLayout(new BorderLayout());
+		
+		nuovoUtente = new JButton("Nuovo utente");
+		utenteEsistente = new JButton("Utente esistente");
+		
+		pan = new JPanel();
+		pan.add(nuovoUtente);
+		
+		pan.add(utenteEsistente);
+		entrata.add(pan, BorderLayout.SOUTH);
+		
+		messaggioEntrata = new JLabel("Benvenuto al Multisala Pancakes", SwingConstants.CENTER);
+		entrata.add(messaggioEntrata, BorderLayout.NORTH);
+		
+		nuovoUtente.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				EntrataNuovoUtente enu = new EntrataNuovoUtente();
+			}
+		});
+	}
+}
