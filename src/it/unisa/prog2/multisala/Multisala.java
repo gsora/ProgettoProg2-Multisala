024d4342cc039982ef02de6d6b2f527e69007adb
Carmine Vincenzo Russo <carmine94@hotmail.it>
2014-12-03 18:46:37 +0100

    Aggiunta classe iniziale per il programma

---
+++ src/it/unisa/prog2/multisala/Multisala.java
@@ -0,0 +1,13 @@
+package it.unisa.prog2.multisala;
+
+public class Multisala {
+
+	public static void main(String[] args){
+	
+		String nomeCinema = "Multisala Pancakes";
+		Entrata pancakes = new Entrata(nomeCinema);
+		
+
+	}
+
+}
