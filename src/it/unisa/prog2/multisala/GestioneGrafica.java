b595f8a140f0bb25d5995397a1f679bfeba37fc9
Gianguido Sorà <gianguidorama@gmail.com>
2014-12-04 15:43:29 +0100

    aggiunto argomento al metodo costruisciUI

--- src/it/unisa/prog2/multisala/GestioneGrafica.java
+++ src/it/unisa/prog2/multisala/GestioneGrafica.java
@@ -1,7 +1,9 @@
 package it.unisa.prog2.multisala;
 
+import javax.swing.JFrame;
+
 public interface GestioneGrafica {
 	
-	public void costruisciUI();
+	public void costruisciUI(JFrame frameChiamante);
 
 }
