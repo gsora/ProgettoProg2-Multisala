55bfd6521fa7901cfb3be7f58e8b6233123c0863
Gianguido Sorà <gianguidorama@gmail.com>
2014-12-17 13:13:24 +0100

    pulizia e riordine generale del codice, finalmente con javadoc e commenti

--- src/it/unisa/prog2/multisala/DBManager.java
+++ src/it/unisa/prog2/multisala/DBManager.java
@@ -3,7 +3,6 @@ package it.unisa.prog2.multisala;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOError;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -12,73 +11,110 @@ import java.util.UUID;
 
 public class DBManager {
 	
+	// numero delle sale da gestire
+	private static final int NUM_SALE = 4;
+	
+	// path dove sono contenuti i file dati
 	private static String cartellaDati;
+	
+	// path del file Database.pks
 	private String pathFileDatabase;
+	
+	// array popolato con i nomi dei file .pks di ogni singola sala
 	private static String[] nomiFileSale;
-	private ArrayList<ArrayList<Spettacolo>> arrayListeSpettPerSala;
+	
+	// arraylist con la lista dei posti prenotati
+	// TODO: controllare il funzionamento del meccanismo, probabilmente è più comodo crearne uno al volo all'interno del metodo
 	private ArrayList<Prenotazione> listaPostiPrenotati;
 	
+	/**
+	 * Costruttore di DBManager
+	 * 
+	 * Automaticamente crea le cartelle nella path appropriata in base al sistema operativo su cui è in esecuzione.
+	 * Basta istanziarlo per accedere al database e ai suoi metodi di scrittura.
+	 * 
+	 */
 	public DBManager() {
 		cartellaDati = new String();
 		nomiFileSale = new String[4];
-		arrayListeSpettPerSala = new ArrayList<ArrayList<Spettacolo>>();
 		
+		// questa stringa contiene il nome del sistema operativo in utilizzo sulla macchina attuale
 		String OS = System.getProperty("os.name").toUpperCase();
+		
 		if(OS.contains("WIN")) {
+			// macchina Windows, si usa AppData
 			cartellaDati = (System.getenv("AppData") + "/MultisalaPancakes");
 		} else if(OS.contains("MAC")) {
+			// macchina OS X, si usa ~/Library/Application Support
 			cartellaDati = (System.getProperty("user.home").toString() + "/Library/Application Support/MultisalaPancakes");
 		} else if(OS.contains("LINUX")) {
+			// macchina Linux, si usa ~/.config/
 			cartellaDati = (System.getProperty("user.home").toString() + "/.config/MultisalaPancakes");
 		}
 		
+		// ora possiamo sapere dove si trova il file di Database
 		pathFileDatabase = cartellaDati + "/" + "Database.pks";
 		
+		// controllo: se cartellaDati esiste carica i nomi delle sale, altrimenti crea la struttura della directory
 		if(controllaEsistenzaPath()) {
-			System.out.println("La path esiste");
 			caricaNomiCartelle();
 		} else {
-			System.out.println("La path non esiste");
 			creaStrutturaDirectory();
 		}
 	}
 	
+	// controlla se esiste già un vecchio database
 	private Boolean controllaEsistenzaPath() {
 		File test = new File(cartellaDati);
 		return test.exists();
 	}
 	
+	// crea la struttura delle directory che serviranno a memorizzare prenotazioni, utenti e film nelle sale
 	private void creaStrutturaDirectory() {
-		// riempi l'arraylist
-		for(int i = 0; i < 4; i++) {
+		
+		// crea un arraylist di arraylist che contengono solo spettacoli, d'appoggio
+		ArrayList<ArrayList<Spettacolo>> arrayListeSpettPerSala = new ArrayList<ArrayList<Spettacolo>>();
+		
+		// riempi l'arraylist in base alle sale
+		for(int i = 0; i < NUM_SALE; i++) {
 			ArrayList<Spettacolo> a = new ArrayList<Spettacolo>();
 			arrayListeSpettPerSala.add(a);
 		}
 		
-		
+		// crea la cartella dei dati nella path definita prima
 		File d = new File(cartellaDati);
 		d.mkdir();
 		
+		// crea la cartella dove verranno contenuti gli utenti e le loro prenotazioni
 		File du = new File(cartellaDati + "/Utenti");
 		du.mkdir();
 		
-		UUID [] sale = new UUID[4];
+		// array di UUID random, creato solo per comodità di iterazione
+		UUID [] sale = new UUID[NUM_SALE];
 		
+		// per ogni elemento nell'array sopracitato... 
 		for (int j = 0; j < sale.length; j++) {
 			try {
+				
+				// ...inizializza l'elemento attuale...
 				sale[j] = UUID.randomUUID();
+				
+				// ...serializza un arraylist<Spettacolo> preso da arrayListeSpettPerSala...
 				FileOutputStream salaX = new FileOutputStream(cartellaDati + "/" + sale[j].toString() + ".pks");
 				ObjectOutputStream salaOut = new ObjectOutputStream(salaX);
 				salaOut.writeObject(arrayListeSpettPerSala.get(j));
 				salaOut.close();
 				salaX.close();
+				
+				// ...per poi aggiungere il suo nome alla lista delle cartelle
 				nomiFileSale[j] = sale[j].toString() + ".pks";
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
+				// TODO: magari c'è da controllare l'eccezione?
 				e.printStackTrace();
 			}
 		}
 		
+		// serializza Database.pks con l'array nomiFileSale 
 		try {
 			FileOutputStream nomiFileSaleSerializzato = new FileOutputStream(pathFileDatabase);
 			ObjectOutputStream outStream = new ObjectOutputStream(nomiFileSaleSerializzato);
@@ -86,41 +122,58 @@ public class DBManager {
 			outStream.close();
 			nomiFileSaleSerializzato.close();
 		} catch (IOException e) {
+			// TODO: magari c'è da controllare l'eccezione?
 			e.printStackTrace();
 		}
 		
 	}
 	
+	// carica la struttura delle directory del database in memoria e rendila utilizzabile 
 	private void caricaNomiCartelle() {
 		try {
+			
+			// deserializza Database.pks in nomiFileSale
 			FileInputStream nomiCartelleSaleSerializzato = new FileInputStream(pathFileDatabase);
 			ObjectInputStream inStream = new ObjectInputStream(nomiCartelleSaleSerializzato);
 			nomiFileSale = (String []) inStream.readObject();
 			inStream.close();
 			nomiCartelleSaleSerializzato.close();
 		} catch (IOException | ClassNotFoundException e) {
+			// TODO: magari c'è da controllare l'eccezione?
 			e.printStackTrace();
 		}
 	}
 	
+	// ritorna la path della sala data in argomento
 	private static String pathSalaNumero(int numeroSala) {
+		// TODO: lanciare un'eccezione se la sala non esiste?
 		return cartellaDati + "/" + nomiFileSale[numeroSala - 1];
 	}
 	
+	/**
+	 * Salva uno spettacolo nel database
+	 * @param s oggetto spettacolo da aggiungere al database
+	 */
+	
 	public void salvaSpettacolo(Spettacolo s) {
+		// apro la sala di riferimento leggendola da s
 		String pathSpettacolo = cartellaDati + "/" + nomiFileSale[s.getNumeroSala() - 1];
 		File fA = new File(pathSpettacolo);
 		
 		try {
+			// deserializzo l'oggetto...
 			FileInputStream fis = new FileInputStream(fA);
 			ObjectInputStream ois = new ObjectInputStream(fis);
 			ArrayList<Spettacolo> als = (ArrayList<Spettacolo>) ois.readObject();
+			// ...lo aggiungo all'arraylist deserializzato als...
 			als.add(s);
 			ois.close();
 			fis.close();
 			
+			// ...elimino il vecchio file per evitare conflitti...
 			fA.delete();
 			
+			// ...serializzo als in un file con lo stesso nome di prima.
 			FileOutputStream fos = new FileOutputStream(fA);
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
 			oos.writeObject(als);
@@ -131,30 +184,36 @@ public class DBManager {
 		}
 	}
 	
-	public void rimuoviSpettacolo(Spettacolo s) {
-		String pathSpettacolo = cartellaDati + "/" + nomiFileSale[s.getNumeroSala() - 1];
-		File fA = new File(pathSpettacolo);
-		
-		try {
-			FileInputStream fis = new FileInputStream(fA);
-			ObjectInputStream ois = new ObjectInputStream(fis);
-			ArrayList<Spettacolo> als = (ArrayList<Spettacolo>) ois.readObject();
-			als.remove(s);
-			ois.close();
-			fis.close();
-			
-			fA.delete();
-			
-			FileOutputStream fos = new FileOutputStream(fA);
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			oos.writeObject(als);
-			oos.close();
-			fos.close();
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+	// LA RIMOZIONE DI UNO SPETTACOLO è DA RIVEDERE, PASSARE UN OGGETTO INTERO DA RIMUOVERE NON è ELEGANTE
+//	public void rimuoviSpettacolo(Spettacolo s) {
+//		String pathSpettacolo = cartellaDati + "/" + nomiFileSale[s.getNumeroSala() - 1];
+//		File fA = new File(pathSpettacolo);
+//		
+//		try {
+//			FileInputStream fis = new FileInputStream(fA);
+//			ObjectInputStream ois = new ObjectInputStream(fis);
+//			ArrayList<Spettacolo> als = (ArrayList<Spettacolo>) ois.readObject();
+//			als.remove(s);
+//			ois.close();
+//			fis.close();
+//			
+//			fA.delete();
+//			
+//			FileOutputStream fos = new FileOutputStream(fA);
+//			ObjectOutputStream oos = new ObjectOutputStream(fos);
+//			oos.writeObject(als);
+//			oos.close();
+//			fos.close();
+//		} catch (IOException | ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//	}
 	
+	/**
+	 * Carica gli spettacoli da una sala
+	 * @param numeroSala numero della sala da cui caricare gli spettacoli
+	 * @return array di Spettacolo contenente gli spettacoli della sala data in argomento
+	 */
 	public static Spettacolo[] caricaSpettacoliInSala(int numeroSala) {
 		File a = new File(pathSalaNumero(numeroSala));
 		ArrayList<Spettacolo> als = new ArrayList<Spettacolo>();
@@ -173,17 +232,31 @@ public class DBManager {
 		return  returnThis;
 	}
 	
-	
+	/**
+	 * Aggiungi un utente al database
+	 * @param userID ID dell'utente
+	 */
 	public void aggiungiUtente(String userID) {
+		// crea una cartella sotto cartellaDati/Utenti con l'userID come nome
 		File user = new File(cartellaDati + "/Utenti/" + userID);
 		user.mkdir();
 	}
 	
+	/**
+	 * Aggiungi una prenotazione per un utente
+	 * @param userID identificativo dell'utente
+	 * @param numeroPosto numero posto da prenotare
+	 * @param numeroSala numero sala in cui prenotare il posto
+	 */
 	public void aggiungiPrenotazione(String userID, int numeroPosto, int numeroSala) {
+		
+		// localizza la cartella dell'utente
 		File user = new File(cartellaDati + "/Utenti/" + userID);
 		
+		// crea una prenotazione p
 		Prenotazione p = new Prenotazione(numeroSala, numeroPosto);
 		
+		// serializzazione della prenotazione
 		try {
 			FileOutputStream f = new FileOutputStream(user);
 			ObjectOutputStream o = new ObjectOutputStream(f);
