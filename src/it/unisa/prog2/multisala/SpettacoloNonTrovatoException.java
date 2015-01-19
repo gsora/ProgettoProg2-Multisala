98ac2f879203e7718fe4ee805c6484fef47d515a
Gianguido Sora' <gianguidorama@gmail.com>
2015-01-19 16:56:31 +0100

    aggiunta eccezione in caso di spettacolo non trovato

---
+++ src/it/unisa/prog2/multisala/SpettacoloNonTrovatoException.java
@@ -0,0 +1,13 @@
+package it.unisa.prog2.multisala;
+
+public class SpettacoloNonTrovatoException extends Exception {
+	
+	public SpettacoloNonTrovatoException() {
+		
+	}
+	
+	public SpettacoloNonTrovatoException(String message) {
+		super(message);
+	}
+
+}
