e064fc1905a0eb3a17a647dffd7033537ab2f0f2
Carmine Vincenzo Russo <carmine94@hotmail.it>
2015-01-22 16:49:29 +0100

    aggiunta sezione grafica alla classe ListaSale

--- src/it/unisa/prog2/multisala/gui/gestione/ListaSale.java
+++ src/it/unisa/prog2/multisala/gui/gestione/ListaSale.java
@@ -2,60 +2,104 @@
 
 package it.unisa.prog2.multisala.gui.gestione;
 
+import it.unisa.prog2.multisala.abstracts.DBManager;
 import it.unisa.prog2.multisala.abstracts.GestioneGrafica;
+import javafx.scene.layout.Border;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ListaSale implements GestioneGrafica {
 	
 	private JFrame listaSale;
-	private JPanel pannelloSuperiore;
-	private JPanel pannelloInferiore;
 	
-	private JLabel titoloFilm;
-	private JLabel durataFilm;
-	private JLabel genereFilm;
-	private JLabel orariFilm;
-	private JLabel giorniFilm;
-	private JLabel numeroSala;
+	private JTable informazioni;
 	
-	private JButton acquistaPrenota;
+	private DefaultTableModel dtm;
 	
-	private JRadioButton sceltaOrdinamentoLista;
+	private JComboBox<String> visualizzaSala;
 	
-	private JComboBox sala1;
-	private JComboBox sala2;
-	private JComboBox sala3;
-	private JComboBox sala4;
-	private JComboBox comboSale;
+	private JButton prenotaAcquista;
+
+	private DBManager dbm;
 	
 	public ListaSale(JFrame frameChiamante) {
 		// inizializzazione listaSale con le proprietà corrette
 		listaSale = new JFrame();
-		listaSale.setSize(500,800);
+		listaSale.setSize(750,800);
 		listaSale.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		listaSale.setLocationRelativeTo(null);
 		listaSale.setTitle("Lista spettacoli");
-		listaSale.setLayout(new GridLayout(2,1));
+		listaSale.setLayout(new BorderLayout());
+		
+		String[] nomiColonne = {"Titolo", "Numero Sala", "Data", "Orario di inizio", "Durata", "Posti liberi", "Sconto"};
+		
+		informazioni = new JTable();
+		dtm = new DefaultTableModel(0, 0) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+		
+		dtm.setColumnIdentifiers(nomiColonne);
+		informazioni.setCellSelectionEnabled(true);
+		informazioni.setModel(dtm);
 		
 		
+		JScrollPane scroll = new JScrollPane(informazioni);
+		listaSale.add(scroll, BorderLayout.CENTER);
 		
+		visualizzaSala = new JComboBox<String>();
+		visualizzaSala.addItem("Programmazione Complessiva");
+		visualizzaSala.addItem("Sala 1");
+		visualizzaSala.addItem("Sala 2");
+		visualizzaSala.addItem("Sala 3");
+		visualizzaSala.addItem("Sala 4");
+		visualizzaSala.setPreferredSize(new Dimension(300, 30));
+		JPanel app0 = new JPanel(new FlowLayout());
+		app0.add(visualizzaSala);
+		listaSale.add(app0, BorderLayout.NORTH);		
+		
+		prenotaAcquista = new JButton("Prenota//Acquista");
+		prenotaAcquista.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+
+				System.out.println(informazioni.getSelectedRow());
+				
+			}
+		});
+		JPanel app1 = new JPanel(new FlowLayout());
+		app1.add(prenotaAcquista);
+		listaSale.add(app1, BorderLayout.SOUTH);
+
+	
 	}
+	
 	
 	public void costruisciUI(JFrame frameChiamante) {
 		listaSale.setVisible(true);
